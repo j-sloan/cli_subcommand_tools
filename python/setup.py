552befbf7888775eca07b5fb02e8c799d639055b
jonny <j.m.sloaneo@gmail.com>
2020-09-05 15:53:23 +0200

    add github URL

--- python/setup.py
+++ python/setup.py
@@ -3,8 +3,9 @@ from setuptools import find_packages, setup
 
 setup(
     name="cli_subcommand_tools",
-    version="0.0.1",
+    version="0.0.2",
     author="jonny sloan",
+    url="https://github.com/j-sloan/cli_subcommand_tools",
     description="A small set of utilities for making subcommand-based CLI interfaces",
     long_description_content_type="text/markdown",
     long_description="see the readme on github",
